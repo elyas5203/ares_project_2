21e1c44f8f3d5d59259d4239540eda3ad7c29c25
elyas5203 <elyas.bahmanabadi@gmail.com>
2025-07-11 13:35:17 +0330

    Delete DabestanSite directory

--- DabestanSite/user/includes/auth_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-// File: DabestanSite/user/includes/auth_check.php
-require_once '../config.php';
-if (!isset($_SESSION['user_id']) || (isset($_SESSION['is_super_admin']) && $_SESSION['is_super_admin'])) {
-    header("Location: login.php");
-    exit;
-}
